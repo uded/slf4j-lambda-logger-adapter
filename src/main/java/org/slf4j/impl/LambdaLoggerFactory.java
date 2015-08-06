dfc91b7bb914efc83a83ef4605bc1c68d29889f9
Jeremy Watkins <jeremy.r.watkins@gmail.com>
2015-08-06 08:26:44 -0700

    Load log level from lambda-logger.xml file on the class path, defaulting to INFO if file cannot be loaded or parsed.

--- src/main/java/org/slf4j/impl/LambdaLoggerFactory.java
+++ src/main/java/org/slf4j/impl/LambdaLoggerFactory.java
@@ -3,6 +3,12 @@ package org.slf4j.impl;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import org.slf4j.ILoggerFactory;
 import org.slf4j.Logger;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 /**
  * @author <a href="mailto:d@davemaple.com">David Maple</a>
@@ -20,8 +26,25 @@ public class LambdaLoggerFactory implements ILoggerFactory {
 	}
 
 	public Logger getLogger(String className) {
-		final LambdaLogLevel logLevel = LambdaLogLevel.INFO;
+		final LambdaLogLevel logLevel = loadFromClassPath();
 		return new LambdaLoggerAdapter(logLevel, threadLocal.get());
+	}
+
+	private static LambdaLogLevel loadFromClassPath() {
+		LambdaLogLevel logLevel = LambdaLogLevel.INFO;
+		final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
+		try {
+			final DocumentBuilder builder = factory.newDocumentBuilder();
+			final Document document = builder.parse(
+					  LambdaLoggerFactory.class.getClassLoader().getResourceAsStream("lambda-logger.xml"));
+			final NodeList nodes = document.getElementsByTagName("level");
+			final Element element = (Element)nodes.item(0);
+			final String level = element.getTextContent();
+			logLevel = LambdaLogLevel.valueOf(level.toUpperCase());
+		} catch (Exception ex) {}
+
+		return logLevel;
 	}
 
 }
